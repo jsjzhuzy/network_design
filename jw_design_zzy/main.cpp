5d109fae46e38e5b7b6f001bc93d5395d3447e97
zhuzy <jsjzhuzy@163.com>
2018-01-08 04:09:15 +0800

    添加初始版本的main.cpp文件

--- jw_design_zzy/main.cpp
+++ jw_design_zzy/main.cpp
@@ -1,79 +1,6 @@
 #include "class.h"
-template<class T,class E>
-void ShortestPath(Graphmtx<T, E>& G, T v,E dist[], int path[])
-{
-	//E dist[], int path[]
-	int n = G.NumberOfVertices();
-	bool *S = new bool[n];
-	int i, j, k;
-	E w, min;
-	for (i = 0; i < n; i++)
-	{
-		//int vs = G.getVertexPos(v);
-		dist[i] = G.getWeight(v, i);
-		S[i] = false;
-		if (i != v&&dist[i] < G.maxWeight)
-		{
-			path[i] = v;
-		}
-		else
-		{
-			path[i] = -1;
-		}
-	}
-	S[v] = true;
-	dist[v] = 0;
-	for (i = 0; i < n - 1; i++)
-	{
-		min = G.maxWeight;
-		int u = v;
-		for (j = 0; j < n; j++)
-		{
-			if (S[j] == false && dist[j] < min)
-			{
-				u = j;
-				min = dist[j];
-			}
-			S[u] = true;
-			for (k = 0; k < n; k++)
-			{
-				w = G.getWeight(u, k);
-				if (S[k] == false && w < G.maxWeight&&dist[u] + w < dist[k])
-				{
-					dist[k] = dist[u] + w;
-					path[k] = u;
-				}
-			}
-		}
-	}
-};
-template<class T,class E>
-void printShortestPath(Graphmtx<T, E>& G, int v, E dist[], int path[])
-{
-	cout << "从路由器" << G.getValue(v) << "到其他各路由器最短路径为：" << endl;
-	int i, j, k, n = G.NumberOfVertices();
-	int *d = new int[n];
-	for (i = 0; i < n; i++)
-	{
-		if (i != v)
-		{
-			j = i;
-			k = 0;
-			while (j != v)
-			{
-				d[k++] = j;
-				j = path[j];
-			}
-			cout << "路由器" << G.getValue(i) << "的最短路径为：" << G.getValue(v);
-			while (k > 0)
-			{
-				cout << G.getValue(d[--k]) << " ";
-				cout << "最短路径长度为：" << dist[i] << endl;
-			}
-		}
-	}
-	delete []d;
-};
+#include "source2.cpp"
+#include "source.cpp"
 template<class T, class E>
 void DFS(Graphmtx<T, E>& G, const T& v)
 {
@@ -167,28 +94,16 @@ int main()
 		fin1 >> ee;
 		one.insertEdge(qq, ww, ee);
 	}
-	//cout << "bbbb" << endl;
-	//cout << "aaa" << endl;
-	//one.insertVertex('a');
-	//one.insertVertex('b');
-	//one.insertVertex('c');
-	//one.insertEdge(0, 1, 4);
-	//one.insertEdge(1, 2, 6);
-	//one.insertVertex('d');
-	//one.insertVertex('e'); 
-	//one.insertEdge(3, 4, 13);
-	//one.insertEdge(1, 4, 4);
-	//one.insertEdge(1, 3, 8);
 	DFS<char, int>(one, 'c');
 	cout << one << endl;
 	int *a, *b;
 	a = (int *)new int[one.NumberOfVertices()];
 	b = (int *)new int[one.NumberOfVertices()];
-	//Floyd<char, int>(one);
-	//int dist[6];
-	//int path[6];
 	ShortestPath<char, int>(one, 0, a, b);
 	printShortestPath<char, int>(one, 0, a, b);
+	getchar();
+	one.removeVertex(0);
+	cout << one << endl;
 	getchar();
 	return 0;
 }
